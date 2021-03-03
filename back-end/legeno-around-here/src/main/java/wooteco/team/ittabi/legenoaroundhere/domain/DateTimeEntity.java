c773e50bc40a6a04e69c8f0d2a53f5411209aca0
aegis <aegis1920@gmail.com>
2021-03-03 15:07:40 +0900

    refactor : BaseEntity를 BaseEntity와 DateTimeEntity로 분리

--- back-end/legeno-around-here/src/main/java/wooteco/team/ittabi/legenoaroundhere/domain/DateTimeEntity.java
+++ back-end/legeno-around-here/src/main/java/wooteco/team/ittabi/legenoaroundhere/domain/DateTimeEntity.java
@@ -2,28 +2,29 @@ package wooteco.team.ittabi.legenoaroundhere.domain;
 
 import java.time.LocalDateTime;
 import javax.persistence.EntityListeners;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import lombok.AccessLevel;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-@EqualsAndHashCode(of = "id", callSuper = false)
 @ToString
-public abstract class BaseEntity extends DateTimeEntity {
+public abstract class DateTimeEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @CreatedDate
+    private LocalDateTime createdAt;
 
-    private LocalDateTime deletedAt;
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
